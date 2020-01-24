c6d725dc2086fc6efef08eeb2549693f2662c795
Tu Anh Nguyen <nguat2018@gmail.com>
2020-01-23 19:50:59 -0800

    Version 1

---
+++ MOVIEList/MovieCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieCellCollectionViewCell.swift
+//  MOVIEList
+//
+//  Created by Tu A Nguyen on 1/23/20.
+//  Copyright © 2020 Tu A Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class MovieCellCollectionViewCell: UICollectionViewCell {
+    
+}
